559b679232428c6e650118644390be244f091bd6
Afasyah <alifya.difa707@gmail.com>
2022-08-28 02:44:11 +0700

    Footer carousel

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => (
       <div className="footer">
          {
             // eslint-disable-next-line prefer-spread
-            Array.apply(null, Array(5)).map((val: null, i: number) => (
+            Array.apply(null, Array(14)).map((val: null, i: number) => (
                <h2 key={i} className="footer__title">
                   SHUSSY!
                </h2>
