5e782f87889d16bd15f139bd258d677cafaa8d2b
Afasyah <alifya.difa707@gmail.com>
2022-08-28 20:14:48 +0700

    fix build & workflow only on new tag

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 /* eslint-disable prettier/prettier */
-const { DefinePlugin } = require('webpack')
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
@@ -13,13 +12,6 @@ module.exports = merge(
       plugins:
       [
          new CleanWebpackPlugin(),
-         new DefinePlugin({
-            'process.env':
-            {
-              'NODE_ENV': JSON.stringify('production'),
-              'API_URL': JSON.stringify('https://jsonplaceholder.typicode.com')
-            }
-         }),
       ],
    }
 )
