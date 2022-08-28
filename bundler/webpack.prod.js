725e32812706273200cbc2d273bf6f626994940c
Afasyah <alifya.difa707@gmail.com>
2022-08-28 20:27:42 +0700

    revert workflow to tags (not using firebase for now)

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 /* eslint-disable prettier/prettier */
+const { DefinePlugin } = require('webpack')
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
@@ -12,6 +13,13 @@ module.exports = merge(
       plugins:
       [
          new CleanWebpackPlugin(),
+         new DefinePlugin({
+            'process.env':
+            {
+              'NODE_ENV': JSON.stringify('production'),
+              'API_URL': JSON.stringify('https://jsonplaceholder.typicode.com')
+            }
+         }),
       ],
    }
 )
